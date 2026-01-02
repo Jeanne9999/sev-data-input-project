8cc69ee09e8511e7f98415d6c77ccfa5c94b5290
Jeanne9999 <i.kotronaki@gmail.com>
2026-01-02 23:38:21 +0200

    chore: install tailwind css

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,13 @@
+const Header = () => {
+
+    return (
+        <>
+            <header className={}>
+
+
+            </header>
+        </>
+    )
+}
+
+export default Header;
---
+++ src/components/layout/Layout.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Header from "./Header.tsx";
+import Footer from "./Footer.tsx";
+
+interface LayoutProps {
+    children: React.ReactNode;
+}
+
+const Layout = ({children}: LayoutProps) => {
+    return (
+        <>
+            {/* HEADER */}
+            <Header />
+            {children}
+
+
+            {/* FOOTER */}
+            <Footer />
+        </>
+    )
+}
+
+export default Layout;
