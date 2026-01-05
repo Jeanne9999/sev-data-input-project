4f5e9c074c587e1fc24221b9f2a4c9a9533daf7d
Jeanne9999 <i.kotronaki@gmail.com>
2026-01-05 15:08:08 +0200

    chore: changed the font to semi-bold in Footer and Header

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -3,7 +3,7 @@ const currentYear = new Date().getFullYear();
     return (
         <>
             <footer className="bg-my-blue">
-                <div className="container mx-auto py-8 text-center">
+                <div className="container mx-auto py-8 text-center font-semibold">
                     &copy; {currentYear} DataEntry. All rights reserved
                 </div>
             </footer>
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -3,7 +3,7 @@ const Header = () => {
     return (
         <>
             <header className={"bg-my-blue fixed w-full"}>
-                <div className={"container mx-auto px-6 flex items-center justify-between"}>
+                <div className={"container mx-auto px-6 flex items-center justify-between font-semibold"}>
                     <img
                         className="h-16 my-4"
                         src="de-logo.jpg"
